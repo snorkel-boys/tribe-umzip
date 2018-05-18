8100ce1c12b0b9d741fa7ac6490bde62eea88930
sj602 <tjswls602@gmail.com>
2018-05-18 17:24:50 +0900

    expressjs

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("node app run");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+
+router.get('/', (err, res) => {
+	res.send('환영합니다.');
+
+});
